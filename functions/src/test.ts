31351ac32a718c908b822980e371ab8fdc93ff67
박수정 <zxzx4342@naver.com>
2024-02-26 18:32:31 +0900

    gitignore 수정

--- functions/src/test.ts
+++
@@ -1,6 +0,0 @@
-import { onSchedule } from "firebase-functions/v2/scheduler";
-import * as logger from "firebase-functions/logger";
-
-export const test = onSchedule("every day 18:00", async () => {
-  logger.info("test functions is start at 18:00");
-});
